e217062048059fefd0c028ab97822a84ace66c14
vinay lalam <89630849+vinaylalam@users.noreply.github.com>
2022-10-05 17:15:05 +0530

    Create ifelseifelse.dart

---
+++ ifelseifelse.dart
@@ -0,0 +1,16 @@
+// there is no if else conditions in dart programming language
+main(){
+var a=null;
+if (a is int){
+print("Integer");
+}
+elseif(a is bool){
+print("bollean");
+}
+else
+print("Happy dusshera");
+}
+/* definitely you will get 
+   happpy dusshera because today is dusshera
+   so HAPPY DUSSHERA
+   */
